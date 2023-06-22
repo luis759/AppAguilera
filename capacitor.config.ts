60e8569f37c8f6f4cbedf1899dffa1e6faedcb10
Luis7592 <luisprador759@gmail.com>
2023-06-22 16:12:41 -0400

    agregadop

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,12 @@ const config: CapacitorConfig = {
   appId: 'com.fuelmatic.ionfulmatic',
   appName: 'Fuelmatic',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  }
 };
 
 export default config;
